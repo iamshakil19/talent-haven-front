d4a49a1907f073b101acf4e5ddb6ef103d945c23
iamshakil19 <shakilahmed.pure@gmail.com>
2024-04-28 01:06:01 +0600

    job list card design

---
+++ src/components/shared/JobListCard.tsx
@@ -0,0 +1,63 @@
+
+import { Card, CardContent } from "../ui/card";
+import { PiBuildingsLight } from "react-icons/pi";
+import { IoLocationOutline } from "react-icons/io5";
+import { BsClock } from "react-icons/bs";
+import { LiaMoneyBillWaveSolid } from "react-icons/lia";
+import { PiBookmarkSimpleThin } from "react-icons/pi";
+const JobListCard = () => {
+  return (
+    <Card className="hover:shadow-lg cursor-pointer hover:shadow-primary-gray/10 transition-all duration-300">
+      <CardContent className="p-7 flex gap-3">
+        <div>
+          <img
+            src="https://superio-reactjs.ibthemespro.com/images/resource/company-logo/1-1.png"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col justify-between gap-4 relative w-full">
+
+          <span className="absolute -right-4 -top-5 hover:bg-primary-gray/20 h-8 w-8 rounded-full transition-all duration-300 ease-in-out hidden md:flex items-center justify-center">
+            <PiBookmarkSimpleThin className="text-lg" />
+          </span>
+
+
+          <p className="font-medium hover:text-primary duration-300 transition-all ease-in-out cursor-pointer max-w-sm">
+            Software Engineer (Android), Libraries
+          </p>
+          <div className="flex items-center gap-4 flex-wrap text-primary-gray">
+            <p className="flex items-center gap-1">
+              <PiBuildingsLight size={19} />
+              <span className="text-sm">Segment</span>
+            </p>
+            <p className="flex items-center gap-1">
+              <IoLocationOutline size={19} />
+              <span className="text-sm">London, UK</span>
+            </p>
+            <p className="flex items-center gap-1">
+              <BsClock />
+              <span className="text-sm">11 hours ago</span>
+            </p>
+            <p className="flex items-center gap-1">
+              <LiaMoneyBillWaveSolid size={19} />
+              <span className="text-sm">35k - 45k</span>
+            </p>
+          </div>
+          <div className="flex h-fit items-center gap-4 flex-wrap">
+            <span className="px-4 py-[5px] tracking-wide rounded-full text-xs bg-secondary-blue text-primary-blue">
+              Full Time
+            </span>
+            <span className="px-4 py-[5px] tracking-wide rounded-full text-xs bg-secondary-green text-primary-green">
+              Private
+            </span>
+            <span className="px-4 py-[5px] tracking-wide rounded-full text-xs bg-secondary-orange text-primary-orange">
+              Urgent
+            </span>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default JobListCard;
--- src/views/public/Home/components/FeaturedJobs/FeaturedJobs.tsx
+++ src/views/public/Home/components/FeaturedJobs/FeaturedJobs.tsx
@@ -3,8 +3,7 @@ import {
   IPopularJobCategory,
   config,
 } from "../PopularCategories/PopularCategories.config";
-import { Card, CardContent } from "@/components/ui/card";
-// import { config } from "./FeaturedJobs.config";
+import JobListCard from "@/components/shared/JobListCard";
 
 const FeaturedJobs = () => {
   return (
@@ -13,22 +12,9 @@ const FeaturedJobs = () => {
       <p className="text-center text-sm mt-5">
         Know your worth and find the job that qualify your life
       </p>
-
-      <div className="mt-20 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-20 grid gap-5 grid-cols-1 md:grid-cols-2">
         {config.PopularJobCategories?.map((item: IPopularJobCategory) => (
-          <div className="group cursor-pointer" key={item.id}>
-            <Card className=" group-hover:shadow-lg group-hover:shadow-primary/5 transition-all duration-300">
-              <CardContent className="p-5 flex items-center gap-5">
-                <div className="p-4 bg-primary/5 text-primary group-hover:bg-primary group-hover:text-primary-foreground rounded-md transition-all duration-300 text-4xl">
-                  {item.icon}
-                </div>
-                <div>
-                  <p className="mb-2 font-semibold">{item.title}</p>
-                  <p className="text-sm">({item.subtitle} open positions)</p>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
+          <JobListCard key={item.id} />
         ))}
       </div>
     </Container>
