53fb0714caafb7120cf5e8f34e4f972ea394aed9
iamshakil19 <shakilahmed.pure@gmail.com>
2024-05-24 02:53:19 +0600

    change env config

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = (): string => {
-  // return process.env.PUBLIC_API_BASE_URL as string;
-  return "https://talent-haven-server.vercel.app/api/v1" as string;
+  return process.env.PUBLIC_API_BASE_URL as string;
+  // return "https://talent-haven-server.vercel.app/api/v1" as string;
 };
